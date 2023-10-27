834260a447d27755818860491bfe2dbcf649fb17
simranrawat <simranrawat2k@gmail.com>
2023-10-27 13:48:08 +0530

    experience

---
+++ aircover.js
@@ -0,0 +1,12 @@
+/*show title "aircover"*/
+const aircoverElement = document.querySelector('.content .aircover');
+const hostElement = document.querySelector('.content .host');
+const paraElement = document.querySelector('.content .aircover-para');
+
+function showElements() {
+    aircoverElement.classList.add('show');
+    hostElement.classList.add('show');
+    paraElement.classList.add('show');
+}
+showElements();
+
